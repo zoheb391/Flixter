26691bf2dd34e4b4653f7cb99e167b0c249c2719
Zoheb <zoheb_391@hotmail.com>
2016-02-18 20:05:52 +0000

    Dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Flixter::Application.routes.draw do
   devise_for :users
   root 'static_pages#index'
+  resource :dashboard, only: [:show]
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
